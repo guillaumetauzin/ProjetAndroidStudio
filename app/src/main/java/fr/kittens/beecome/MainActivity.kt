02aae984ffecc5213ef1f9184fde263ffb820b57
Eiche Tristan <tristaneiche@gmail.com>
2021-12-26 10:29:42 +0100

    Mise en place de la page d'accueil avec deux boutton, un header. Ajout du code de redirection vers une nouvelle page (Ne manque plus que les créer)

--- app/src/main/java/fr/kittens/beecome/MainActivity.kt
+++ app/src/main/java/fr/kittens/beecome/MainActivity.kt
@@ -1,11 +1,26 @@
 package fr.kittens.beecome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonzone1 = findViewById<Button>(R.id.button_zone_1)
+        val buttonzone2 = findViewById<Button>(R.id.button_zone_2)
+        buttonzone1.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application,GroupeInfoActivity::class.java)
+            startActivity(newIntent)
+        })
+
+        buttonzone2.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application,RayonsActivity::class.java)
+            startActivity(newIntent)
+        })
+
     }
 }
