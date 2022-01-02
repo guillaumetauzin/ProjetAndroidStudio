1fd9b4d0b1196e5a85aeddf926f40c29eead70f2
Felix <felix.robinmores@epsi.fr>
2022-01-02 15:36:41 +0100

    Import dans la zone 2 du kotlin + implementation dans le gradle de picasso et okhttp3

--- app/src/main/java/fr/kittens/beecome/zone2.kt
+++ app/src/main/java/fr/kittens/beecome/zone2.kt
@@ -1,11 +1,22 @@
 package fr.kittens.beecome
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
 
 class zone2 : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zone2)
     }
+
+    'okHttpClient.newCall(request).enqueue(object : Callback {
+        'override fun onFailure(call: Call, e: IOException) {
+            'TODO("Not yet implemented")
+        '}
 }
