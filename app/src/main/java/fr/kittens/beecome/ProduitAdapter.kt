eca4580512bf6dd0c3447ba18608302827334191
Guillaume <guillaumetauzin11@gmail.com>
2022-01-02 19:14:16 +0100

    creation ProduitAdapter.kt

---
+++ app/src/main/java/fr/kittens/beecome/ProduitAdapter.kt
@@ -0,0 +1,43 @@
+package fr.kittens.beecome
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import fr.kittens.beecome.ProduitAdapter.ViewHolder
+
+class ProduitAdapter(val produit: ArrayList<Produit>): RecyclerView.Adapter<ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val textViewName = view.findViewById<TextView>(R.id.textCategorie)
+        //val textViewEmail = view.findViewById<TextView>(R.id.textViewEmail)
+        //val textViewCity = view.findViewById<TextView>(R.id.textViewCity)
+        val contentLayout = view.findViewById<LinearLayout>(R.id.contentLayout2)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.cell_categorie, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val categorie = produit.get(position)
+        holder.textViewName.text=categorie.name
+        //holder.textViewEmail.text=student.products_url
+        //holder.textViewCity.text=student.title
+
+        holder.contentLayout.setOnClickListener(View.OnClickListener {
+            Log.d("URL des produits", categorie.picture_url)
+
+            //(holder.contentLayout.context.applicationContext as AppKittens).showToast(categorie.products_url)
+        })
+    }
+
+    override fun getItemCount(): Int {
+        return produit.size
+    }
+}
---
+++ app/src/main/java/fr/kittens/beecome/ZoneProduit.kt
@@ -0,0 +1,65 @@
+package fr.kittens.beecome
+
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
+
+class ZoneProduit : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zone2)
+        setHeaderTitle("Rayons")
+        showBack()
+
+        val categories = arrayListOf<Categorie>()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewCat)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val categoriesAdapter = CategorieAdapter(categories)
+        recyclerView.adapter=categoriesAdapter
+
+        val okHttpClient: OkHttpClient = OkHttpClient.Builder().build()
+        val mRequestURL="https://djemam.com/epsi/categories.json"
+        val request = Request.Builder()
+            .url(mRequestURL)
+            .get()
+            .cacheControl(CacheControl.FORCE_NETWORK)
+            .build()
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val data = response.body?.string()
+
+                if(data!=null){
+                    val jsStudents = JSONObject(data)
+                    val jsArrayStudents= jsStudents.getJSONArray("items")
+                    for(i in 0 until jsArrayStudents.length()){
+                        val jsStudent = jsArrayStudents.getJSONObject(i)
+                        val categorie = Categorie(jsStudent.optString("category_id",""),
+                            jsStudent.optString("title",""),
+                            jsStudent.optString("products_url",""))
+                        categories.add(categorie)
+                        Log.d("Categorie ",categorie.title)
+                    }
+                    Log.d("Student","${categories.size}")
+
+                    //
+
+                    runOnUiThread(Runnable {
+                        categoriesAdapter.notifyDataSetChanged()
+                    })
+                }
+            }
+
+        })
+    }
+}
